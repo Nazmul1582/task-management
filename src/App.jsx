ae54ce90361e3b56ca06ee5ff630abaf80d77195
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-21 10:42:16 +0600

    created some pages and set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Outlet } from "react-router-dom"
+
 function App() {
 
   return (
     <>
-      <h1 className="text-center font-bold text-5xl">Task Management System</h1>
+      <Outlet />
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-center font-bold text-5xl">Task Management System</h1>
+            <h2>This is the Home Page</h2>            
+        </div>
+    );
+};
+
+export default Home;
