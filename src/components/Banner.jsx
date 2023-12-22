6ade4f45c34fb53475fdd9ed09e98b776d545af9
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-22 14:59:43 +0600

    created a banner section using daisyui

---
+++ src/components/Banner.jsx
@@ -0,0 +1,24 @@
+import image from "../assets/task-management.jpg";
+
+const Banner = () => {
+  return (
+    <div className="hero min-h-screen">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 justify-center items-center">
+        <div>
+          <h1 className="text-5xl font-bold">
+            Super charge your productivity
+          </h1>
+          <p className="py-6">
+            Discover a smarter way to manage tasks. ProTaskManager is your
+            all-in-one solution for seamless task organization, collaboration,
+            and productivity. Experience a new era of efficiency today.
+          </p>
+          <button className="btn btn-accent">Let&apos;s Explore</button>
+        </div>
+        <img src={image} className="rounded-lg" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
