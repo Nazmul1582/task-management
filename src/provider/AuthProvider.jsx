00a25f399209d8674121b6d73505fa4dcd168515
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-23 18:29:20 +0600

    created a AuthProvider and wrapped all components with it

---
+++ src/provider/AuthProvider.jsx
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+export const AuthContext = createContext();
+const authInfo = {}
+
+const AuthProvider = ({children}) => {
+    return <AuthContext.Provider value={authInfo}>
+        {children}
+    </AuthContext.Provider>
+};
+
+export default AuthProvider;
