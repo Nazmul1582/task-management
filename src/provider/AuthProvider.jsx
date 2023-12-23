aee279085de83ca51577cbf77cb7ecafabef381c
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-23 18:44:56 +0600

    installed firebase and set up firebase config and created createUser functionality

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,7 +1,18 @@
 import { createContext } from "react";
+import app from "../firebase/firebase.config";
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
 
 export const AuthContext = createContext();
-const authInfo = {}
+const auth = getAuth(app);
+
+const createUser = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+} 
+
+
+const authInfo = {
+    createUser
+}
 
 const AuthProvider = ({children}) => {
     return <AuthContext.Provider value={authInfo}>
