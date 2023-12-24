106fe949d2fb72e29f8a6f6b4e19c52dda20b89d
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-24 09:43:35 +0600

    login functionality added using react-hook-form and show toast

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,30 @@
 import { Link } from "react-router-dom";
 import googleImage from "../assets/google.png";
 import githubImage from "../assets/github-logo.png";
+import useAuth from "../hooks/useAuth";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 const Login = () => {
+  const { login } = useAuth();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const { email, password } = data;
+
+    // login user
+    login(email, password)
+      .then(() => {
+        toast.success("User has loggin in successfully.");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  };
   return (
     <div className="hero min-h-screen bg-base-100 py-10">
       <div className="hero-content flex-col">
@@ -15,7 +37,7 @@ const Login = () => {
           </p>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -24,8 +46,11 @@ const Login = () => {
                 type="email"
                 placeholder="email"
                 className="input input-bordered"
-                required
+                {...register("email", { required: true })}
               />
+              {errors.email && (
+                <p className="text-sm text-red-500">Email is required!</p>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
@@ -35,8 +60,16 @@ const Login = () => {
                 type="password"
                 placeholder="password"
                 className="input input-bordered"
-                required
+                {...register("password", { required: true, minLength: 6 })}
               />
+              {errors.password?.type === "required" && (
+                <p className="text-sm text-red-500">Password is required!</p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p className="text-sm text-red-500">
+                  Password must be 6 characters or longer.
+                </p>
+              )}
             </div>
             <div className="form-control mt-6">
               <button className="btn btn-accent">Login</button>
