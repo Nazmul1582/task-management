5dd5e42fe90d6dfa803a18ebe1b4876580acab50
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-24 07:18:09 +0600

    created the onAuthStateChanged observer and set user

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,30 +1,55 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import app from "../firebase/firebase.config";
-import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  updateProfile,
+} from "firebase/auth";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
-const createUser = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password)
-} 
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-const updateUser = (username, image) => {
-    return updateProfile(auth.currentUser, {
-        displayName: username, 
-        photoURL: image
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+        setLoading(false)
     })
-}
+    return unsubscribe;
+  }, [])
 
-const authInfo = {
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUser = (username, image) => {
+    return updateProfile(auth.currentUser, {
+      displayName: username,
+      photoURL: image,
+    });
+  };
+
+  const authInfo = {
+    user,
     createUser,
-    updateUser
-}
+    updateUser,
+  };
 
-const AuthProvider = ({children}) => {
-    return <AuthContext.Provider value={authInfo}>
-        {children}
+  return (
+    <AuthContext.Provider value={authInfo}>
+      {loading ? (
+        <div className="flex justify-center items-center min-h-screen">
+          <span className="loading loading-bars loading-lg"></span>
+        </div>
+      ) : (
+        children
+      )}
     </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
